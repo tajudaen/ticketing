42312dc3bd8d9b7e17726c8ab41a31990880945e
Taju Giwa <giwa208@gmail.com>
2022-02-15 18:43:55 +0100

    start up

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { OrderCreatedListener } from "./events/listener/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+	console.log("starting");
 	if (!process.env.NATS_CLIENT_ID)
 		throw new Error("NATS_CLIENT_ID must be defined");
 	if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined");
